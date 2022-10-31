ec956a1f845d89ec278a4ed85a29559006808660
Mike Weatherford <mikesweatherford@gmail.com>
2022-10-31 18:32:09 -0400

    moved menu solutions to a different folder to clean some stuff up

--- Code Kentucky Semester One Final Project/Testing.cs
+++ Code Kentucky Semester One Final Project/Testing.cs
@@ -6,6 +6,7 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Code_Kentucky_Semester_One_Final_Project
 {
     internal class Start
@@ -15,213 +16,62 @@ namespace Code_Kentucky_Semester_One_Final_Project
 
 
             string url = "https://raw.githubusercontent.com/GiorgioMotorola/Rate-Your-Music-Top-500-JSON/main/JSON";
+            string? pick = Utility.MainMenu();
 
             HttpClient client = new HttpClient();
+        while(pick != "0")
+            {
 
+            
             try
             {
                 var httpResponseMessage = await client.GetAsync(url);
                 string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
                 var myPosts = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+
                 
 
-
-                Console.WriteLine("Hello! Make a selection below: ");
-                Console.WriteLine("1. Return all albums 1 - 500");
-                Console.WriteLine("2. Return Top 10");
-                Console.WriteLine("3. Return all Rock related genres");
-                Console.WriteLine("4. Return all Metal related genres");
-                Console.WriteLine("5. Return all Hip Hop related genres");
-                var pick = Console.ReadLine();
-                Console.Beep(650, 100);
-
-
-                foreach (var post in myPosts)
-                {
-
+            foreach (var post in myPosts)
+             {
+                                 
                     if (pick == "1")
                     {
-                        
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"{post.position}. {post.artist} - {post.name}");
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Genre: {post.genres}");
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"User Rating: {post.rating} || Number of User Ratings: {post.num_ratings} || Number of User Reviews: {post.num_reviews}");
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"Year Released: {post.date}");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("____________________________________________________________________________________________________");
-                        Console.WriteLine("===================================================================================================|");
-                        Console.WriteLine("___________________________________________________________________________________________________|");
-                        
-
+                        Utility.SelectionOne(post);
                     }
+
                     else if (pick == "2")
                     {
                         if (post.position <= 10)
                         {
-                            
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.WriteLine($"{post.position}. {post.artist} - {post.name}");
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Genre: {post.genres}");
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine($"User Rating: {post.rating} || Number of User Ratings: {post.num_ratings} || Number of User Reviews: {post.num_reviews}");
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine($"Year Released: {post.date}");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine("____________________________________________________________________________________________________");
-                            Console.WriteLine("===================================================================================================|");
-                            Console.WriteLine("___________________________________________________________________________________________________|");
+                            Utility.SelectionTwo(post);
                         }
                     }
-                    else if (pick == "3" )
+                    else if (pick == "3")
                     {
+                        Utility.SelectionThree(myPosts);
 
-                        List<string> artistLists = new List<string>();
-                        List<string> genresList = new List<string>();
-                        List<string> nameList = new List<string>();
-
-                        foreach (var get in myPosts)
-                        {
-                            var position = (int)get.position;
-                            string? artist = get.artist;
-                            string? genres = get.genres;
-                            string? names = (string?)get.name;
-                            string[] strings = genres.Split(",");
-                            foreach (string s in strings)
-                            {
-                                if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
-                                {
-                                    artistLists.Add(s);
-                                    genresList.Add(s);
-                                    nameList.Add(s);    
-
-                                    if (s.Contains("Rock"))
-                                    {
-                                        Console.WriteLine($"{position}. Artist: {artist} - {names}");
-                                        Console.WriteLine($"Genre: {s}");
-                                        Console.WriteLine("____________________________________________________________________________________________________");
-                                        Console.WriteLine("===================================================================================================|");
-                                        Console.WriteLine("___________________________________________________________________________________________________|");
-
-                                    }
-                                }
-                                
-
-
-                            }
-
-                        }
-
-                    } 
+                    }
                     else if (pick == "4")
                     {
-                        
-                        List<string> artistLists = new List<string>();
-                        List<string> genresList = new List<string>();
-                        List<string> nameList = new List<string>();
-
-                        foreach (var get in myPosts)
-                        {
-                            var position = (int)get.position;
-                            string? artist = get.artist;
-                            string? genres = get.genres;
-                            string? names = (string?)get.name;
-                            string[] strings = genres.Split(",");
-                            
-                            foreach (var s in strings)
-                            {
-                                if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
-                                {
-                                    artistLists.Add(s);
-                                    genresList.Add(s);
-                                    nameList.Add(s);
-                                    
-
-                                    if (s.Contains("Metal"))
-                                    {
-                                        Console.WriteLine($"{position}. Artist: {artist} - {names}");
-                                        Console.WriteLine($"Genre: {s}");
-                                        Console.WriteLine("____________________________________________________________________________________________________");
-                                        Console.WriteLine("===================================================================================================|");
-                                        Console.WriteLine("___________________________________________________________________________________________________|");
-
-                                    }
-                                    
-                                }
-                                
-
-
-
-                            }
-
-                        }
+                        Utility.SelectionFour(myPosts);
                     }
                     else if (pick == "5")
                     {
+                        Utility.SelectionFive(myPosts);
 
-                        List<string> artistLists = new List<string>();
-                        List<string> genresList = new List<string>();
-                        List<string> nameList = new List<string>();
-
-                        foreach (var get in myPosts)
-                        {
-                            var position = (int)get.position;
-                            string? artist = get.artist;
-                            string? genres = get.genres;
-                            string? names = (string?)get.name;
-                            string[] strings = genres.Split(",");
-
-                            foreach (var s in strings)
-                            {
-                                if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
-                                {
-                                    artistLists.Add(s);
-                                    genresList.Add(s);
-                                    nameList.Add(s);
-
-
-                                    if (s.Contains("Hip Hop"))
-                                    {
-
-                                        
-                                        Console.WriteLine($"{position}. Artist: {artist} - {names}");
-                                        Console.WriteLine($"Genre: {s}");
-                                        Console.WriteLine("____________________________________________________________________________________________________");
-                                        Console.WriteLine("===================================================================================================|");
-                                        Console.WriteLine("___________________________________________________________________________________________________|");
-
-                                    }
-
-                                }
-
-
-
-
-                            }
-
-                        }
                     }
-
                     
                 }
-                
-                Console.ReadLine();
-    
-    
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-
             }
-            finally
-            {
-                client.Dispose();
-            }
-            
+         finally
+       {
+              client.Dispose();
+             }           
+          }
         }
     }
 }
---
+++ Code Kentucky Semester One Final Project/Utility.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Kentucky_Semester_One_Final_Project
+{
+    public class Utility
+    {
+        public static string? MainMenu()
+        {
+            Console.WriteLine("Hello! Make a selection below: ");
+            Console.WriteLine("1. Return all albums 1 - 500");
+            Console.WriteLine("2. Return Top 10");
+            Console.WriteLine("3. Return all Rock related genres");
+            Console.WriteLine("4. Return all Metal related genres");
+            Console.WriteLine("5. Return all Hip Hop related genres");
+            Console.WriteLine("0. Quit");
+            var pick = Console.ReadLine();
+            Console.Beep(650, 100);
+            return pick;
+        }
+
+        public static void SelectionOne(Post post)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{post.position}. {post.artist} - {post.name}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Genre: {post.genres}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"User Rating: {post.rating} || Number of User Ratings: {post.num_ratings} || Number of User Reviews: {post.num_reviews}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Year Released: {post.date}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("____________________________________________________________________________________________________");
+            Console.WriteLine("===================================================================================================|");
+            Console.WriteLine("___________________________________________________________________________________________________|");
+        }
+
+        public static void SelectionTwo(Post post)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{post.position}. {post.artist} - {post.name}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Genre: {post.genres}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"User Rating: {post.rating} || Number of User Ratings: {post.num_ratings} || Number of User Reviews: {post.num_reviews}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Year Released: {post.date}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("____________________________________________________________________________________________________");
+            Console.WriteLine("===================================================================================================|");
+            Console.WriteLine("___________________________________________________________________________________________________|");
+        }
+
+        public static void SelectionThree(Post[] myPosts)
+        {
+            List<string> artistLists = new List<string>();
+            List<string> genresList = new List<string>();
+            List<string> nameList = new List<string>();
+
+            foreach (var get in myPosts)
+            {
+                var position = (int?)get.position;
+                string? artist = get.artist;
+                string? genres = get.genres;
+                string? names = (string?)get.name;
+                string[] strings = genres.Split(",");
+                foreach (string s in strings)
+                {
+                    if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
+                    {
+                        artistLists.Add(s);
+                        genresList.Add(s);
+                        nameList.Add(s);
+
+                        if (s.Contains("Rock"))
+                        {
+                            Console.WriteLine($"{position}. Artist: {artist} - {names}");
+                            Console.WriteLine($"Genre: {s}");
+                            Console.WriteLine("____________________________________________________________________________________________________");
+                            Console.WriteLine("===================================================================================================|");
+                            Console.WriteLine("___________________________________________________________________________________________________|");
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        public static void SelectionFour(Post[] myPosts)
+        {
+            List<string> artistLists = new List<string>();
+            List<string> genresList = new List<string>();
+            List<string> nameList = new List<string>();
+
+            foreach (var get in myPosts)
+            {
+                var position = (int?)get.position;
+                string? artist = get.artist;
+                string? genres = get.genres;
+                string? names = (string?)get.name;
+                string[] strings = genres.Split(",");
+
+                foreach (var s in strings)
+                {
+                    if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
+                    {
+                        artistLists.Add(s);
+                        genresList.Add(s);
+                        nameList.Add(s);
+
+
+                        if (s.Contains("Metal"))
+                        {
+                            Console.WriteLine($"{position}. Artist: {artist} - {names}");
+                            Console.WriteLine($"Genre: {s}");
+                            Console.WriteLine("____________________________________________________________________________________________________");
+                            Console.WriteLine("===================================================================================================|");
+                            Console.WriteLine("___________________________________________________________________________________________________|");
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        public static void SelectionFive(Post[] myPosts)
+        {
+            List<string> artistLists = new List<string>();
+            List<string> genresList = new List<string>();
+            List<string> nameList = new List<string>();
+
+            foreach (var get in myPosts)
+            {
+                var position = (int?)get.position;
+                string? artist = get.artist;
+                string? genres = get.genres;
+                string? names = (string?)get.name;
+                string[] strings = genres.Split(",");
+
+                foreach (var s in strings)
+                {
+                    if (!genresList.Contains(s) && !artistLists.Contains(s) && !nameList.Contains(s))
+                    {
+                        artistLists.Add(s);
+                        genresList.Add(s);
+                        nameList.Add(s);
+
+
+                        if (s.Contains("Hip Hop"))
+                        {
+
+
+                            Console.WriteLine($"{position}. Artist: {artist} - {names}");
+                            Console.WriteLine($"Genre: {s}");
+                            Console.WriteLine("____________________________________________________________________________________________________");
+                            Console.WriteLine("===================================================================================================|");
+                            Console.WriteLine("___________________________________________________________________________________________________|");
+
+
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
